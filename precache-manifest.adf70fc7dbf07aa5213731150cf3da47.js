1d8d0404b5688d2d204279b30a3b025ec865e74c
smket99 <f20180231@hyderabad.bits-pilani.ac.in>
2020-05-28 02:32:56 +0530

    Updates

---
+++ precache-manifest.adf70fc7dbf07aa5213731150cf3da47.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3791f16fb605eb93e8e6c75429f632af",
+    "url": "/login-form/index.html"
+  },
+  {
+    "revision": "2385bd0f00036b1868d0",
+    "url": "/login-form/static/css/main.1eaa7671.chunk.css"
+  },
+  {
+    "revision": "49426df33a9ff9a543ac",
+    "url": "/login-form/static/js/2.18204dca.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/login-form/static/js/2.18204dca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2385bd0f00036b1868d0",
+    "url": "/login-form/static/js/main.e43c7024.chunk.js"
+  },
+  {
+    "revision": "ab5c499e42deac23e76c",
+    "url": "/login-form/static/js/runtime-main.19023b81.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/login-form/static/media/logo.5d5d9eef.svg"
+  }
+]);
