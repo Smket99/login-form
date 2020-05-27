a754d7e0797eae24985a64b3e900b8385c966bc0
smket99 <f20180231@hyderabad.bits-pilani.ac.in>
2020-05-28 05:06:24 +0530

    Updates

--- precache-manifest.7c49e48139defa2c2836241423924c4e.js
+++ precache-manifest.7c49e48139defa2c2836241423924c4e.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2ae950397e584e5a0ebcecab3f25b258",
+    "revision": "c1862d4c547a866ea559b9e3e5da9cb0",
     "url": "/login-form/index.html"
   },
   {
-    "revision": "0b34faeb643c6b18e905",
+    "revision": "47d16b487595cb3d027e",
     "url": "/login-form/static/css/main.8e5fb882.chunk.css"
   },
   {
-    "revision": "aee793db5a95e928b725",
-    "url": "/login-form/static/js/2.4e1b9e8c.chunk.js"
+    "revision": "7be0d0af23f6afdd1cc5",
+    "url": "/login-form/static/js/2.4c59628c.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/login-form/static/js/2.4e1b9e8c.chunk.js.LICENSE.txt"
+    "url": "/login-form/static/js/2.4c59628c.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "0b34faeb643c6b18e905",
-    "url": "/login-form/static/js/main.e1c413fe.chunk.js"
+    "revision": "47d16b487595cb3d027e",
+    "url": "/login-form/static/js/main.595af562.chunk.js"
   },
   {
     "revision": "ccbf62ecb731e49b9420",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/login-form/precache-manifest.b824342d93a394c8b2ef5edffc4e98ee.js"
+  "/login-form/precache-manifest.7c49e48139defa2c2836241423924c4e.js"
 );
 
 self.addEventListener('message', (event) => {
