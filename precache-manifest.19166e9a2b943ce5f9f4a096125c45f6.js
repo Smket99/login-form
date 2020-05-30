404e143a468ce57f03fc2124de3c7a95e69a1afb
smket99 <f20180231@hyderabad.bits-pilani.ac.in>
2020-05-30 18:37:32 +0530

    Updates

---
+++ precache-manifest.19166e9a2b943ce5f9f4a096125c45f6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "582d13f9188e280e817a26799958f9fd",
+    "url": "/login-form/index.html"
+  },
+  {
+    "revision": "f94edbf3c0803c372316",
+    "url": "/login-form/static/css/main.21de1089.chunk.css"
+  },
+  {
+    "revision": "f2c4ce3fa45d40f0031a",
+    "url": "/login-form/static/js/2.4462b94d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/login-form/static/js/2.4462b94d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f94edbf3c0803c372316",
+    "url": "/login-form/static/js/main.e16fd9d0.chunk.js"
+  },
+  {
+    "revision": "ab5c499e42deac23e76c",
+    "url": "/login-form/static/js/runtime-main.19023b81.js"
+  },
+  {
+    "revision": "a7d0db35cb693a4bb54e93cac3b7995b",
+    "url": "/login-form/static/media/background2.a7d0db35.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/login-form/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0a691cd425d394b50a9271a8a1dc148b",
+    "url": "/login-form/static/media/nav-bar.0a691cd4.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2bebba61f6234d4f1bbd62802c9a9d01.js"
+  "/login-form/precache-manifest.19166e9a2b943ce5f9f4a096125c45f6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/login-form/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
